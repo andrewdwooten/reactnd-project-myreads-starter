b3f703b1c964579ed4581b00ca259a6b30fe9202
andrewdwooten <andy@biggerpockets.com>
2021-02-06 10:49:13 -0700

    refactor add BookShelfChanger component
    Adds BookShelfChanger component, extracting displayed icon and menu dropdown to same. THis
    should allow for easier understanding and future editing when working with the application

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,12 @@
 import React from 'react'
-
+import BookShelfChanger from './BookShelfChanger'
 function Book( { book } ) {
 	return (
  		<li>
 			<div className="book">
 			  <div className="book-top">
 			    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLink})` }}></div>
-			    <div className="book-shelf-changer">
-			      <select>
-			        <option value="move" disabled>Move to...</option>
-			        <option value="currentlyReading">Currently Reading</option>
-			        <option value="wantToRead">Want to Read</option>
-			        <option value="read">Read</option>
-			        <option value="none">None</option>
-			      </select>
-			    </div>
+					<BookShelfChanger />
 			  </div>
 			  <div className="book-title">{book.title}</div>
 			  { book.authors.map((authorName) => (
