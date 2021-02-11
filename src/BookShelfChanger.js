200c92c428477930ff5db5bad5b610ba72ca57ea
andrewdwooten <andy@biggerpockets.com>
2021-02-11 06:45:49 -0700

    refactor Make bookshelfchanger functional component
    Again, no state management or lifecycle events are required, so a stateless component is indicated. Follow-up change for feedback

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class BookShelfChanger extends Component {
-  static propTypes = {
-    handleUpdate: PropTypes.func.isRequired,
-    currentShelf: PropTypes.string
-  }
+function BookShelfChanger({ handleUpdate, currentShelf }) {
 
-	handleChange = (event) => {
-    this.props.handleUpdate(event.target.value)
+	const handleChange = (event) => {
+    handleUpdate(event.target.value)
 	};
 
-	render() {
-		const { currentShelf } = this.props
-
-		return (
-	    <div className="book-shelf-changer">
-		      <select
-		      	onChange={this.handleChange}
-		      	value={currentShelf || "none"}>
-		        <option value="move" disabled>Move to...</option>
-		        <option value="currentlyReading">Currently Reading</option>
-		        <option value="wantToRead">Want to Read</option>
-		        <option value="read">Read</option>
-		        <option value="none">None</option>
-		      </select>
-		    </div>
-		)
-	}
+	return (
+    <div className="book-shelf-changer">
+	      <select
+	      	onChange={handleChange}
+	      	value={currentShelf || "none"}>
+	        <option value="move" disabled>Move to...</option>
+	        <option value="currentlyReading">Currently Reading</option>
+	        <option value="wantToRead">Want to Read</option>
+	        <option value="read">Read</option>
+	        <option value="none">None</option>
+	      </select>
+	    </div>
+	)
 }
+
+BookShelfChanger.propTypes = {
+  handleUpdate: PropTypes.func.isRequired,
+  currentShelf: PropTypes.string
+}
+
 
 export default BookShelfChanger;
